f51e4fe843ff99690554b3e86573350e3129280a
sydney-jovian <126050345+sydney-jovian@users.noreply.github.com>
2023-04-18 09:44:24 +0000

    add static files

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+    res.sendFile(path.join(__dirname, 'pages/index.html'));
+})
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Server running on https://localhost:${port}`);
-});
+    console.log(`Server running on https://localhost:${port}`)
+})
